26dc42329d4acc82a7caf940515677ecf49b5390
SamLee <goha2010@naver.com>
2023-06-01 23:24:13 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.js'
-import './index.css'
-
-ServiceWorkerRegistration.register()
+import App from './App.js';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+root.render(<App />);
 
-
-
+ServiceWorkerRegistration.register();
